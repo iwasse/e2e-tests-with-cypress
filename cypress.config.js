c9ef568cec2cf3e11c1de3e763d79939e0f6af10
iwasse <guilherme.iwasse@gmail.com>
2023-09-01 15:45:16 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,12 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: 'https://notes-serverless-app.com',
     defaultCommandTimeout: 20000,
     env: {
       viewportWidthBreakpoint: 768,
     },
   },
-  chromeWebSecurity: false
+  chromeWebSecurity: false,
+  projectId: 'o74dn4'
 })
